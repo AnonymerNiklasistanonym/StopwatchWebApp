cfcd2d96f9121a20ad8e540dc07402dd8b7b441c
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2018-11-08 12:21:43 +0100

    Removed unnecessary image sizes

--- favicons/createIcoFavicons.sh
+++ favicons/createIcoFavicons.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Before using this script you need to execute `createPngFavicons.sh`!
-magick convert favicon_512.png -define icon:auto-resize=128,96,64,48,32,16 favicon.ico
+magick convert favicon_512.png -define icon:auto-resize=64,48,32,16 favicon.ico
