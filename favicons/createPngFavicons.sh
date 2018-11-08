ede33a825356df7b0523ccc6fcbec68776eeaab7
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2018-11-08 11:54:47 +0100

    Added web app manifest and .png icons

--- favicons/createPngFavicons.sh
+++ favicons/createPngFavicons.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Declare an array with the image sizes to export
-array=( 64 128 256 512 )
+array=( 64 128 192 256 512 )
 for i in "${array[@]}"
 do
 	# Export each size as a `png` favicon from `favicon.svg`
